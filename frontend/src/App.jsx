c8aae011a38646c84972290d8172a20c5f894879
asad5059 <asadpatiya3@gmail.com>
2025-02-02 22:40:08 +0600

    Update landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,16 @@ import { Container, Card, Button } from 'react-bootstrap';
 import './App.css';
 
 const BlogPost = ({ blog }) => (
-  <Card className="mb-4">
-    <Card.Body>
-      <Card.Title as="h2" className="mb-3">{blog.title}</Card.Title>
+  <Card className="mb-4 shadow-sm">
+    <Card.Body className="p-4 p-md-5">
+      <Card.Title as="h2" className="mb-4 fs-1">{blog.title}</Card.Title>
       <Card.Text 
         as="div"
         className="blog-content"
         dangerouslySetInnerHTML={{ __html: blog.content }}
       />
       <div className="text-end mt-4 pt-3 border-top">
-        <Button variant="outline-primary">Read More</Button>
+        <Button variant="outline-primary" size="lg">Read More</Button>
       </div>
     </Card.Body>
   </Card>
@@ -37,7 +37,7 @@ function App() {
     <div className="min-vh-100 bg-light">
       <Navbar onCreateBlogClick={() => setShowBlogForm(true)} />
       
-      <Container className="py-4" style={{ marginTop: '80px' }}>
+      <Container fluid className="py-4 px-3 px-md-5" style={{ marginTop: '80px', maxWidth: '1400px' }}>
         {showBlogForm ? (
           <Card>
             <Card.Header className="d-flex justify-content-between align-items-center">
